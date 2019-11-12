e3f4ec42968eb9d91edd0df6eb8ee8ecea26dc92
harry <zhangxinfeng@doorder.com>
2019-11-12 21:53:47 +0800

    sql文件替换

--- public/xy.sql
+++ public/xy.sql
@@ -1,5 +1,5 @@
 ﻿# Host: localhost  (Version: 5.5.53)
-# Date: 2019-10-24 16:20:27
+# Date: 2019-11-12 21:53:32
 # Generator: MySQL-Front 5.3  (Build 4.234)
 
 /*!40101 SET NAMES utf8 */;
@@ -13,17 +13,12 @@ CREATE TABLE `xy_brand` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL COMMENT '名称',
   `number` varchar(255) DEFAULT NULL COMMENT '货号',
-  `img_url` varchar(255) DEFAULT NULL COMMENT '图片地址',
-  `type` int(11) DEFAULT NULL COMMENT '1：衣服；2：裤子；3：鞋子；4：配饰；5：其他',
+  `pic` varchar(255) DEFAULT NULL COMMENT '图片地址',
+  `type` tinyint(3) DEFAULT '1' COMMENT '1：衣服；2：裤子；3：鞋子；4：配饰；5：其他',
   `remark` varchar(255) DEFAULT NULL COMMENT '备注',
   `create_time` int(11) DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='品牌款式表';
-
-#
-# Data for table "xy_brand"
-#
-
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COMMENT='品牌款式表';
 
 #
 # Structure for table "xy_clothe"
@@ -51,9 +46,34 @@ CREATE TABLE `xy_clothe` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='衣服订单表';
 
 #
-# Data for table "xy_clothe"
+# Structure for table "xy_menu"
 #
 
+DROP TABLE IF EXISTS `xy_menu`;
+CREATE TABLE `xy_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='菜单表';
+
+#
+# Structure for table "xy_menu_role"
+#
+
+DROP TABLE IF EXISTS `xy_menu_role`;
+CREATE TABLE `xy_menu_role` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色菜单表';
+
+#
+# Structure for table "xy_role"
+#
+
+DROP TABLE IF EXISTS `xy_role`;
+CREATE TABLE `xy_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
 
 #
 # Structure for table "xy_shoe"
@@ -64,7 +84,7 @@ CREATE TABLE `xy_shoe` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL COMMENT '用户id',
   `brand_id` int(11) DEFAULT NULL COMMENT '品牌id',
-  `shop_type` int(11) DEFAULT NULL COMMENT '1:毒；2：nice;3:绿叉；4：其他',
+  `shop_type` varchar(255) DEFAULT NULL COMMENT '购入平台',
   `size` varchar(255) DEFAULT NULL COMMENT '尺码',
   `buy_price` decimal(10,2) DEFAULT NULL COMMENT '入手价',
   `sale_price` decimal(10,2) DEFAULT NULL COMMENT '出手价',
@@ -73,12 +93,7 @@ CREATE TABLE `xy_shoe` (
   `sale_time` int(11) DEFAULT NULL COMMENT '出手时间',
   `create_time` int(11) DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='球鞋订单表';
-
-#
-# Data for table "xy_shoe"
-#
-
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COMMENT='球鞋订单表';
 
 #
 # Structure for table "xy_user"
@@ -89,16 +104,12 @@ CREATE TABLE `xy_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL COMMENT '用户名',
   `password` varchar(500) DEFAULT NULL COMMENT '密码',
+  `role_id` int(11) DEFAULT NULL COMMENT '角色id',
   `nickname` varchar(255) DEFAULT NULL COMMENT '昵称',
+  `headimg` varchar(255) DEFAULT NULL COMMENT '头像',
   `user_type` tinyint(3) DEFAULT NULL COMMENT '1:admin;2:普通会员',
   `last_login_ip` varchar(255) DEFAULT NULL COMMENT '上次登录ip地址',
   `last_login_time` int(11) DEFAULT NULL COMMENT '上次登录时间',
   `create_time` int(11) DEFAULT NULL COMMENT '注册时间',
-  `ship_fee` decimal(10,2) DEFAULT NULL COMMENT '运费',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='管理员表';
-
-#
-# Data for table "xy_user"
-#
-
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='管理员表';
